6bd294d136fd5e98a64352a82a6af5a07c893050
YazeedEld <yazeed_eldigair@yahoo.com>
2022-02-14 23:54:44 +0400

    connecting to db

--- db.php
+++ db.php
@@ -1,6 +1,12 @@
 <?php
-$conn = mysqli_connect('localhost','root','');
-$database = mysqli_select_db($conn, 'users_bso');
-
-$encodedData = file_get_contents('php://input');  // take data from react native fetch API
-$decodedData = json_decode($encodedData, true);
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+?>
